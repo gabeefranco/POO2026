1cd9c137911412e29e9d9e32adef517c62ac1404
Gabriel Franco <gabefgonc@gmail.com>
2026-08-05 13:47:33 +0000

    first hello-world project

---
+++ helloworld/src/main/java/com/gabeefranco/App.java
@@ -0,0 +1,10 @@
+package com.gabeefranco;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(
+            "Hello, world! Projeto helloworld criado com sucesso."
+        );
+    }
+}
